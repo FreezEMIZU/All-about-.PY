3169a5e6faefa140497627347e70ca840022e82f
E*5 <124773824+FreezEMIZU@users.noreply.github.com>
2023-03-03 14:39:57 +0800

    Delete dictionary[patient].py

--- dictionary[patient].py
+++
@@ -1,8 +0,0 @@
-patient_data = {}
-
-while True:
-    name = input("Enter Patient's name: ").upper()
-    if name in patient_data:
-        print(f'{name} is {patient_data[name]} years old')
-    else:
-        patient_data[name] = input("Enter Patient's age: ")
